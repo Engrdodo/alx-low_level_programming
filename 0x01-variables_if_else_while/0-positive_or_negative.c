b2a21a34ae964f36b55752367d2d586e2d677ace
Akinola David <akinoladavidolawale@gmail.com>
2022-09-09 21:55:12 -0700

    prints Hexa alphabets in lowercase

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 /**
- * main - determine if a number is positive or negative
+ * main - determine if number is positive or negative
  *
  * Description: program will assign a random number to the variable n each time
- * it is executed
+ * it is executed.
  *
  * Return: Always 0 (Success)
  */
@@ -16,17 +15,14 @@ int main(void)
 	int n;
 
 
-
-
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n == 0)
 		printf("%d is zero\n", n);
-	else if (n > 0)
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
 		printf("%d is positive\n", n);
-	else 
-		printf("%d is nagative\n", n);
 
 	return (0);
 }
