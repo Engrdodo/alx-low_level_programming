a53fd60ec0488bd14bf8e323b4ba2a6bc1926cd4
Akinola David Olawale <akinoladavidolawale@gmail.com>
2023-12-25 13:41:26 +0100

    returns absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_abs - returns absolute value of an integer
+ *@n: the integer to be tested
+ *Return: return 0
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	return (n);
+}
+
