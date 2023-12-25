79229a731e2c1e49595e71ab200f881d304d78f6
Akinola David Olawale <akinoladavidolawale@gmail.com>
2023-12-25 10:05:50 +0100

    prints lowercase letters ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints lowercase alphabet
+ * ten times
+ *Description: The function uses while and for loops
+ *Return: No return
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int letter;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar(10);
+	}
+}
