6883ed18338e7f222dbf7b59df7f53c01b0e6b42
Akinola David Olawale <akinoladavidolawale@gmail.com>
2023-12-25 10:42:43 +0100

    prints lowercase letters ten times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,12 +10,12 @@ void print_alphabet_x10(void)
 	int i;
 	int letter;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 		{
 			_putchar(letter);
 		}
-		_putchar(10);
+		_putchar('\n');
 	}
 }
