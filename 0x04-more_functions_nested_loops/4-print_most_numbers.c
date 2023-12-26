42fece33041c78cdf4c14d20c9bc3a755f435f62
Akinola David Olawale <akinoladavidolawale@gmail.com>
2023-12-26 18:04:39 +0100

    prints 0-9 except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,17 +6,16 @@
  * except 2 and 4
  * Return: void
  */
-int print_most_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
 	for (i = 48; i < 58; i++)
 	{
-		if ((i != 50) || (i != 52))
+		if ((i != 50) && (i != 52))
 		{
-			continue;
+			_putchar(i);
 		}
-		_putchar(i);
 	}
 	_putchar(10);
 }
