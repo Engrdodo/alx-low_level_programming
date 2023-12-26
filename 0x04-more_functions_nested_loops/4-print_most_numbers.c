7b6e9f88f21544d74b46129fb004db9830a57bae
Akinola David Olawale <akinoladavidolawale@gmail.com>
2023-12-26 17:59:29 +0100

    prints 0-9 except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,11 +12,12 @@ int print_most_numbers(void)
 
 	for (i = 48; i < 58; i++)
 	{
-		if (i != 50 && i != 52)
+		if ((i != 50) || (i != 52))
 		{
-			_putchar(i + '0');
+			continue;
 		}
+		_putchar(i);
 	}
-	_putchar('\n');
+	_putchar(10);
 }
 
