f0cf21a9605eca7733f9260fff1968b734628ea3
Akinola David Olawale <akinoladavidolawale@gmail.com>
2024-02-10 08:46:15 +0100

    reverse a string

---
+++ 0x05-pointers_arrays_strings/python1.py
@@ -0,0 +1,19 @@
+print("Ola is a programmer")
+print('Learning Python is good')
+
+print('Python course for "Beginners"')
+print("I'm a footballer")
+message = """
+Hi Josh,
+       You are welcome to today's lesson
+ Thank You
+ The Support Team
+"""
+print(message)
+
+print(' I am in pry "six" ')
+print('I am in pry "six" ')
+
+input comment
+
+
